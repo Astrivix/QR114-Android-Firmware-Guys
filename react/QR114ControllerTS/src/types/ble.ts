eb3453a9b5dbbc325fb0f055dba6c4cba0969371
Sasanda Saumya <sasadasaumya@gmail.com>
2025-09-11 17:38:04 +0530

    update

--- react/QR114ControllerTS/src/types/ble.ts
+++
@@ -1,8 +0,0 @@
-// src/types/ble.ts
-import { Characteristic, Service } from "react-native-ble-plx";
-
-// This custom type will hold a service and its characteristics together.
-export interface ServiceWithCharacteristics {
-  service: Service;
-  characteristics: Characteristic[];
-}
